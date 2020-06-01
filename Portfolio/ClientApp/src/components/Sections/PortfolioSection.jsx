d5c501a725c62a33bdb958b77389508628aa7dae
Stefan Alexandru <stefanalexandru02@outlook.com>
2020-06-02 02:53:09 +0300

    last publish from laptop - not much, just changed a year

--- Portfolio/ClientApp/src/components/Sections/PortfolioSection.jsx
+++ Portfolio/ClientApp/src/components/Sections/PortfolioSection.jsx
@@ -8,7 +8,7 @@ export default function PortfolioSection() {
             <div className="row" data-aos="flip-left" data-aos-duration="1500">
                 <div className="col-md-4">
                     <h4>eTomorrowSchool - eLearning Platform</h4>
-                    <span class="badge badge-secondary">2019 - 2020</span>
+                    <span className="badge badge-secondary">2019 - 2020</span>
                     <br/>                    
                     <p>
                         eTomorrowSchool is an A.I. based eLearning platform that helps students learn better, faster, and allows teachers to work with larger classes, easier.
@@ -25,7 +25,7 @@ export default function PortfolioSection() {
                 </div>
                 <div className="col-md-4">
                     <h4>eTomorrowSchool - online library</h4>
-                    <small class="badge badge-secondary">2019</small>
+                    <small className="badge badge-secondary">2019</small>
                     <br/>
                     <p>
                         eTomorrowSchool - online library is a SaaS application for school libraries, that makes managing books, lendings and students easier.
@@ -36,10 +36,10 @@ export default function PortfolioSection() {
             <div className="row" data-aos="flip-left" data-aos-duration="1500">
                 <div className="col-md-4">
                     <h4>"Gheorghe Vranceanu" National College</h4>
-                    <span class="badge badge-secondary">2017 - 2018</span>
+                    <span className="badge badge-secondary">2018</span>
                     <br/>
                     <p>
-                    A custom CMS developed using ASP.NET Core MVC and SQLServer to build and manage "Gheorghe Vranceanu" National College website.
+                        A custom CMS developed using ASP.NET Core MVC and SQLServer to build and manage "Gheorghe Vranceanu" National College website.
                     </p>
                 </div>
                 <div className="col-md-8">
@@ -52,7 +52,7 @@ export default function PortfolioSection() {
                 </div>
                 <div className="col-md-4">
                     <h4>Aerostar Visitors Management System</h4>
-                    <span class="badge badge-secondary">2017</span>
+                    <span className="badge badge-secondary">2017</span>
                     <p>
                         Visitors management system that registers visitors securely, makes a better first immpression, handles all the paperwork, generates automatically reports, and notifies the company's assigned personnell when the visitors have arrived.
                     </p>
