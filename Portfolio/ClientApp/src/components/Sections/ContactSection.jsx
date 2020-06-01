eef8b9554305dd2f6140d8bbab3541290bb27e09
Stefan Alexandru <stefanalexandru02@outlook.com>
2020-06-02 01:41:13 +0300

    contact form UI + remove github & linkedin icons from homesection

--- Portfolio/ClientApp/src/components/Sections/ContactSection.jsx
+++ Portfolio/ClientApp/src/components/Sections/ContactSection.jsx
@@ -4,8 +4,33 @@ export default function ContactSection() {
     return (
         <div>
             <h1 className="text-center">Contact</h1>
-            <br/>            
-            
+            <br/>
+            <div className="row">
+                <div className="col-md-2"></div>
+                <div className="col-md-8">
+                    <div className="form-group" data-aos="fade-up" data-aos-delay="150">
+                        <label>Name:</label>
+                        <input className="form-control form-control-transparent text-light no-outline" placeholder="Name"></input>
+                    </div>            
+                    <div className="form-group" data-aos="fade-up" data-aos-delay="150">
+                        <label>Email:</label>
+                        <input className="form-control form-control-transparent text-light no-outline" placeholder="Email"></input>
+                    </div>            
+                    <div className="form-group" data-aos="fade-up" data-aos-delay="150">
+                        <label>Message:</label>
+                        <textarea className="form-control form-control-transparent text-light no-outline" placeholder="Message" rows={5}></textarea>
+                    </div>   
+                    <br/>
+                    <div className="text-center">
+                        <button className="btn btn-outline-primary no-outline hvr-wobble-horizontal">
+                            <span>Send message</span>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-arrow-right"><line x1="5" y1="12" x2="19" y2="12"></line><polyline points="12 5 19 12 12 19"></polyline></svg>
+                        </button>    
+                    </div>
+                </div>
+                <div className="col-md-2"></div>
+            </div>
+            <br/>
         </div>
     );
 }
